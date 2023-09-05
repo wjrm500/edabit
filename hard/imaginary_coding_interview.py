f043c04a7d241916e4d1d78620a756906ba64838
William May <wjrm500@gmail.com>
2023-09-05 22:42:56 +0100

    Adjusting code

--- hard/imaginary_coding_interview.py
+++ hard/imaginary_coding_interview.py
@@ -22,13 +22,14 @@ max_time_by_difficulty = {
 }
 
 def interview(lst: list[int], tot: int) -> str:
-    qualified = len(lst) == len(q_difficulties) and \
-        all(
-            t <= max_time_by_difficulty[mt]
-            for t, mt in zip(lst, q_difficulties)
-        ) and \
-        tot <= 120
-    return Result.QUALIFIED if qualified else Result.DISQUALIFIED
+    all_qs_completed = len(lst) == len(q_difficulties)
+    if not all_qs_completed:
+        return Result.DISQUALIFIED
+    qs_completed_in_time = all(
+        t <= max_time_by_difficulty[mt]
+        for t, mt in zip(lst, q_difficulties)
+    ) and tot <= 120
+    return Result.QUALIFIED if qs_completed_in_time else Result.DISQUALIFIED
 
 assert interview([5, 5, 10, 10, 15, 15, 20, 20], 120) == "qualified"
 assert interview([2, 3, 8, 6, 5, 12, 10, 18], 120) == "qualified"
