5f1a60f9c2acaff43d42a3c79268897abcab5261
William May <wjrm500@gmail.com>
2023-09-05 22:40:38 +0100

    Imaginary coding interview question

---
+++ hard/imaginary_coding_interview.py
@@ -0,0 +1,42 @@
+from enum import Enum
+
+class Difficulty(Enum):
+    VERY_EASY = 1
+    EASY = 2
+    MEDIUM = 3
+    HARD = 4
+
+class Result:
+    QUALIFIED = "qualified"
+    DISQUALIFIED = "disqualified"
+
+q_difficulties = (d := Difficulty) and [
+    d.VERY_EASY, d.VERY_EASY, d.EASY, d.EASY, d.MEDIUM, d.MEDIUM, d.HARD, d.HARD
+]
+
+max_time_by_difficulty = {
+    d.VERY_EASY: 5,
+    d.EASY: 10,
+    d.MEDIUM: 15,
+    d.HARD: 20
+}
+
+def interview(lst: list[int], tot: int) -> str:
+    qualified = len(lst) == len(q_difficulties) and \
+        all(
+            t <= max_time_by_difficulty[mt]
+            for t, mt in zip(lst, q_difficulties)
+        ) and \
+        tot <= 120
+    return Result.QUALIFIED if qualified else Result.DISQUALIFIED
+
+assert interview([5, 5, 10, 10, 15, 15, 20, 20], 120) == "qualified"
+assert interview([2, 3, 8, 6, 5, 12, 10, 18], 120) == "qualified"
+assert interview([2, 3, 8, 6, 5, 12, 10, 18], 64) == "qualified"
+assert interview([5, 5, 10, 10, 25, 15, 20, 20], 120) == "disqualified"
+assert interview([5, 5, 10, 10, 15, 15, 20], 120) == "disqualified"
+assert interview([5, 5, 10, 10, 15, 15, 20, 20], 130) == "disqualified"
+assert interview([5, 5, 10, 10, 15, 20, 50], 160) == "disqualified"
+assert interview([5, 5, 10, 10, 15, 15, 40], 120) == "disqualified"
+assert interview([10, 10, 15, 15, 20, 20], 150) == "disqualified"
+assert interview([5, 5, 10, 20, 15, 15, 20, 20], 140) == "disqualified"
