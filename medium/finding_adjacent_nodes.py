e02e4fcc93fb085ebbc9d9a159752fa975f2ced7
William May <wjrm500@gmail.com>
2023-09-06 09:10:05 +0100

    Consistency

--- medium/finding_adjacent_nodes.py
+++ medium/finding_adjacent_nodes.py
@@ -6,25 +6,21 @@ def adjacent(
 ) -> bool:
     return adjacency_matrix[node_pair[0]][node_pair[1]]
 
-def test():
-    matrix_1 = [
-        [ 0, 1, 0, 0 ],
-        [ 1, 0, 1, 1 ],
-        [ 0, 1, 0, 1 ],
-        [ 0, 1, 1, 0 ]
-    ]
-    assert adjacent(matrix_1, (0, 1))
-    assert not adjacent(matrix_1, (0, 2))
+matrix_1 = [
+    [ 0, 1, 0, 0 ],
+    [ 1, 0, 1, 1 ],
+    [ 0, 1, 0, 1 ],
+    [ 0, 1, 1, 0 ]
+]
+assert adjacent(matrix_1, (0, 1))
+assert not adjacent(matrix_1, (0, 2))
 
-    matrix_2 = [
-        [ 0, 1, 0, 1, 1 ],
-        [ 1, 0, 1, 0, 0 ],
-        [ 0, 1, 0, 1, 0 ],
-        [ 1, 0, 1, 0, 1 ],
-        [ 1, 0, 0, 1, 0 ]
-    ]
-    assert adjacent(matrix_2, (0, 3))
-    assert not adjacent(matrix_2, (1, 4))
-
-if __name__ == "__main__":
-    test()
+matrix_2 = [
+    [ 0, 1, 0, 1, 1 ],
+    [ 1, 0, 1, 0, 0 ],
+    [ 0, 1, 0, 1, 0 ],
+    [ 1, 0, 1, 0, 1 ],
+    [ 1, 0, 0, 1, 0 ]
+]
+assert adjacent(matrix_2, (0, 3))
+assert not adjacent(matrix_2, (1, 4))
--- medium/virtual_dac.py
+++ medium/virtual_dac.py
@@ -1,2 +1,6 @@
-def virtual_dac(value):
+def virtual_dac(value: float) -> float:
     return round(value / 1023 * 5, 2)
+
+assert virtual_dac(0) == 0
+assert virtual_dac(1023) == 5
+assert virtual_dac(400) == 1.96
